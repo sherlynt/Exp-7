88a7dfe015de2b1f14d4beadba66539fab0e8dee
sherlynt <tiwarisherlyn@gmail.com>
2024-08-25 23:33:11 +0530

    Create input_array.cpp

---
+++ input_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the array: ";
+    cin >> size;
+   
+    int array[size];
+
+    cout << "Enter " << size << " elements:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+    cout << "The elements of the array are:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
