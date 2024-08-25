dc8de8b3b71f239ebaba578b9147acddc8c0cfed
sherlynt <tiwarisherlyn@gmail.com>
2024-08-25 23:32:53 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string s,t;
+    cout<<"Enter first string:"<<endl;
+    cin>>s;
+    cout<<"Enter second string:"<<endl;
+    cin>>t;
+    cout<<"Concatenation of strings is:"<<s+t<<endl;
+    return 0;
+}
