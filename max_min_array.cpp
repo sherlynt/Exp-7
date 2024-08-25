b9e2ac108fb1bbb1145c9e84971edeab995671f3
sherlynt <tiwarisherlyn@gmail.com>
2024-08-25 23:33:57 +0530

    Create max_min_array.cpp

---
+++ max_min_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int array[size];
+
+    cout << "Enter " << size << " elements:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+
+    int max = array[0];
+    int min = array[0];
+
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    cout << "Maximum value in the array: " << max << endl;
+    cout << "Minimum value in the array: " << min << endl;
+
+    return 0;
+}
