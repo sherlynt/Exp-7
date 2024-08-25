4de9d0829f8f079d916f66bc935eeb7478dd3073
sherlynt <tiwarisherlyn@gmail.com>
2024-08-25 23:32:31 +0530

    Create array_declarations.cpp

---
+++ array_declarations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1[5] = {9, 3, 8, 6, 7}; 
+    int a2[] = {2, 1, 6, 3, 4}; 
+    int a3[5] = {8, 6, 7};
+
+ 
+  cout << "\n Traditional Method: ";
+  for (int i = 0; i < 5; ++i) {
+    cout << a3[i] << "  ";
+  }
+  cout << endl;
+
+
+
+   return 0;
+}
