f4125a1e460b6ff91dcb1d59b5fd20db60cbe0a6
sherlynt <tiwarisherlyn@gmail.com>
2024-08-25 23:35:47 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter a string:"<<endl;
+    cin>>s;
+    cout<<"Entered string is:"<<s<<endl;
+    return 0;
+    
+}
